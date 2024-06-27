85d1a347d3357bf30d753a512246f70a531f08e6
Mahsander_yseeva <117755252+MahsaHaghnevis@users.noreply.github.com>
2024-06-27 11:34:56 +0330

    Add files via upload

--- greedy.cpp
+++ greedy.cpp
@@ -9,87 +9,136 @@
 #include <algorithm>
 using namespace std;
 
-struct Cell{
-   int x , y;
-   int GoldBag;
+struct Cell {
+    int x, y;
+    int GoldBag;
 };
 
-bool compareCell(const Cell& a, const Cell& b){
+bool compareCell(const Cell& a, const Cell& b) {
     return a.GoldBag > b.GoldBag;
 }
 
-bool isNumber(const string& str){
-    for(int i=0 ; i<str.length() ;i++){
-        if(!isdigit(str[i]))
-        return false;
-        else
-        return true;
+bool isNumber(const string& str) {
+    for (int t = 0; t < str.size(); ++t){
+        if (!isdigit(str[t])) {
+            return false;
+        }
     }
+    return true;
 }
 
-int GreedyPath(vector <vector <string> >& mapMatrics , int size){
-    int i=0;
-    int j=0;
-    int GoldBag=0;
-    bool PreviousHadRubber=false;
-    while (i<size-1 || j<size-1 )//while it didn't hit the wall
-    {
-        if(isNumber(mapMatrics[i][j])){
-            PreviousHadRubber=false;
-            GoldBag+=stoi(mapMatrics[i][j]);
-        }
-        else if(mapMatrics[i][j]=="!"){
-            PreviousHadRubber=true;  // and also check the house we are in has rubber
-        }
+int GreedyPath(vector<vector<string> >& mapMatrix, int size) {
+    int i = 0, j = 0;
+    int GoldBag = 0;
+    bool PreviousHadRubber = false;
 
-        vector <Cell> Candidates ; //houses of sourrounding that gives feasible ans (right and down houses)
-        if(i<size-1 && mapMatrics[i+1][j]!="X"){
-            int GoldVal=0;
-            if(isNumber(mapMatrics[i+1][j])){  //putting the right one if availabe
-                GoldVal=stoi(mapMatrics[i+1][j]);
-                if(PreviousHadRubber){
-                    GoldVal=-GoldVal; // so in this case we lose the money but we handle it in sorting
-                } 
+    if (mapMatrix[0][0] == "X") { // in case the starting house is blocked
+        cout << "Moving impossible" << endl;
+        return 0;
+    }
+
+    while (i < size - 1 || j < size - 1) { // while it didn't hit the wall
+        if (isNumber(mapMatrix[i][j])) {
+            if (PreviousHadRubber) {
+                GoldBag -= stoi(mapMatrix[i][j]);
+            } else {
+                GoldBag += stoi(mapMatrix[i][j]);
             }
-            Candidates.push_back({i+1,j,GoldVal});
-        }
-        else if(j<size-1 && mapMatrics[i][j+1]!="X"){
-            int GoldVal=0;
-            if(isNumber(mapMatrics[i][j+1])){  //putting the down one if availabe
-                GoldVal=stoi(mapMatrics[i][j+1]);
-                if(PreviousHadRubber){
-                    GoldVal=-GoldVal; // so in this case we lose the money but we handle it in sorting
-                } 
+            PreviousHadRubber = false;
+        } else if (mapMatrix[i][j] == "!") {
+            if (PreviousHadRubber) {
+                // If we encounter two consecutive rubber cells, don't subtract anything
+                PreviousHadRubber = true;
+            } else {
+                PreviousHadRubber = true; // Mark that we've encountered a rubber cell
             }
-            Candidates.push_back({i,j+1,GoldVal});
         }
-        //but if we can't move break loop
-        if(Candidates.empty()==true)
-        break;
 
-        //now we sort them based on the max gold value
-        sort(Candidates.begin()  , Candidates.end() , compareCell);
+        vector<Cell> Candidates; // valid surrounding houses (right and down houses)
 
-        //here choose the best option with the most vgold value and move to it
-        i=Candidates[0].x;
-        j=Candidates[0].y;
-        //now also handle losing the gold of this house
-        if(mapMatrics[i][j]=="!"){
-             GoldBag -= PreviousHadRubber ? 0 : (isNumber(mapMatrics[i][j]) ? stoi(mapMatrics[i][j]) : 0);
+        // Checking the down cell
+        if (i < size - 1 && mapMatrix[i + 1][j] != "X") {
+            int GoldVal = 0;
+            if (isNumber(mapMatrix[i + 1][j])) { // getting the gold value if available
+                GoldVal = stoi(mapMatrix[i + 1][j]);
+                if (PreviousHadRubber) { // if we have rubber, we lose the gold
+                    GoldVal = -GoldVal;
+                }
+            }
+            Cell NewCell;
+            NewCell.x=i+1;
+            NewCell.y=j;
+            NewCell.GoldBag=GoldVal;
+            Candidates.push_back(NewCell);
+        }
+
+        // Checking the right cell
+        if (j < size - 1 && mapMatrix[i][j + 1] != "X") {
+            int GoldVal = 0;
+            if (isNumber(mapMatrix[i][j + 1])) { // getting the gold value if available
+                GoldVal = stoi(mapMatrix[i][j + 1]);
+                if (PreviousHadRubber) {
+                    GoldVal = -GoldVal; // lose the gold if we have rubber
+                }
+            }
+            Cell NewCell;
+            NewCell.x=i;
+            NewCell.y=j+1;
+            NewCell.GoldBag=GoldVal;
+            Candidates.push_back(NewCell);
+        }
+
+        // if no valid moves, break the loop
+        if (Candidates.empty()) {
+            break;
+        }
+
+        // sort candidates based on the max gold value
+        sort(Candidates.begin(), Candidates.end(), compareCell);
+
+        // choose the best option with the most gold value and move to it
+        i = Candidates[0].x;
+        j = Candidates[0].y;
+
+        // if moving to a rubber cell, mark it and subtract the gold of the previous house if not a rubber
+        if (mapMatrix[i][j] == "!" && !PreviousHadRubber) {
+            if (isNumber(mapMatrix[i][j])) {
+                GoldBag -= stoi(mapMatrix[i][j]);
+            }
+            PreviousHadRubber = true;
+        } else {
+            PreviousHadRubber = false;
         }
     }
-    //now add the value of new house
-    if(isNumber(mapMatrics[i][j])){
-        GoldBag+=stoi(mapMatrics[i][j]);
+
+    // add the value of the last house
+    if (isNumber(mapMatrix[i][j])) {
+        GoldBag += stoi(mapMatrix[i][j]);
     }
 
     return GoldBag;
 }
 
-
     
 
 int main(){
+    /*
+    int size;
+    cout<<"Enter the size of the matrix: "<<endl;
+    cin>>size;
+
+    vector<vector<string> > matrix ;
+    for(size_t i=0 ; i<size ; i++){
+        vector<string> row;
+        for(size_t j=0 ; j<size ; j++){
+            string value;
+            cout<<"Enter the value of the cell: "<<endl;
+            cin>>value;
+            row.push_back(value);
+        }
+        matrix.push_back(row);
+    }
+    */
     vector<vector<string> > matrix = {
         {"2", "X", "!", "5", "0"},
         {"1", "9", "!", "X", "3"},
@@ -97,6 +146,7 @@ int main(){
         {"2", "X", "5", "3", "X"},
         {"8", "4", "2", "!", "1"}
     };
+
 
     cout << "Gold collected by greedy path: " << GreedyPath(matrix , 5) << endl;
 
